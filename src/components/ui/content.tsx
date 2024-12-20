11294d0cfc2200c0b4d7669935b46382f29149dd
Garv Thakral <garvthakralfx@gmail.com>
2024-12-20 22:43:28 +0530

    tweet

--- src/components/ui/content.tsx
+++ src/components/ui/content.tsx
@@ -1,25 +1,49 @@
-interface ContentProps{
-    type:string;
-    content:String;
-    link:string;
+import { TwitterTweetEmbed } from "react-twitter-embed";
+
+interface ContentProps {
+  type: string;
+  content: string;
+  link: string;
 }
-function hello(props:ContentProps){
-    console.log(props.type)
-    console.log(props.link.replace('/watch?v=', '/embed/'))
-    if(props.type == "Video"){
-        return <iframe  src={props.link.replace('youtu.be/', 'youtube.com/embed/').replace('/watch?v=', '/embed/')} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-    }else if(props.type == "Tweet"){
-        return <blockquote className="twitter-tweet ">
-        <a href={props.link.replace("x","twitter")}></a>
-      </blockquote>
-    }else{
-        return <a href = {props.link} className = {"text-purple-600 w-full break-words"}><div className = {"py-2"}>{props.link}</div></a>
+
+function renderContent(props: ContentProps) {
+  if (props.type === "Video") {
+    const embedUrl = props.link
+      .replace("youtu.be/", "youtube.com/embed/")
+      .replace("/watch?v=", "/embed/");
+
+    return (
+      <iframe
+        src={embedUrl}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    );
+  } else if (props.type === "Tweet") {
+    const tweetIdMatch = props.link.match(/status\/(\d+)/);
+    if (tweetIdMatch) {
+      const tweetId = tweetIdMatch[1];
+      return <TwitterTweetEmbed tweetId={tweetId} />;
+    } else {
+      return <div>Invalid Tweet URL</div>;
     }
+  } else {
+    return (
+      <a href={props.link} className="text-purple-600 w-full break-words">
+        <div className="py-2">{props.link}</div>
+      </a>
+    );
+  }
 }
-export function Content(props:ContentProps){
-    return<div className = {"flex flex-col justify-between p-2"}>
-        <div className = {"py-2"}>{props.content}</div>
-        {hello(props)}
-        
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="flex flex-col justify-between p-2">
+      <div className="py-2">{props.content}</div>
+      {renderContent(props)}
     </div>
+  );
 }
